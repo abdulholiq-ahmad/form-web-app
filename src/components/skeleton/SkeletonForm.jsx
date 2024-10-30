a986f1394d04c9487d9cdcd9e0f4d9d34f2f11ff
Ahmadbekov Abdulholiq <abdulholiq571@gmail.com>
2024-10-30 12:48:00 +0500

    add: conatiner to skeleton

--- src/components/skeleton/SkeletonForm.jsx
+++ src/components/skeleton/SkeletonForm.jsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const SkeletonForm = () => {
   return (
-    <div className="flex py-16 justify-center w-full h-screen">
+    <div className="container flex py-16 justify-center w-full h-screen">
       <div className="w-[470px] flex flex-col items-center gap-5">
         <Skeleton className="w-full h-[250px] rounded-xl" />
         <Skeleton className=" w-full h-[150px] rounded-xl" />
